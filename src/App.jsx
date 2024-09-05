a31274da85bda8615d8417e455d9f2a78bddc8f0
rishika-2225 <chughrishika709@gmail.com>
2024-09-05 14:07:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Weather from './components/Weather'
+// i am doing this to pull the changes
 
 const App = () => {
   return (
     <div className='app'>
      <Weather /> 
     </div>
+   
   )
 }
 
